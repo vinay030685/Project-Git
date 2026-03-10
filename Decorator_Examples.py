cf2d57fce3c41ec6d19a7deb6ef900e586546c7b
vinay030685 <vinay030685@gmail.com>
2026-03-10 16:47:08 -0400

    Page1

--- Decorator_Examples.py
+++ Decorator_Examples.py
@@ -11,3 +11,35 @@ def say_hello():
     print("Hello!")
 
 say_hello()
+
+# with arguments
+
+def do_twice(func):
+    def wrapper(*args, **kwargs):
+        func(*args, **kwargs)
+        return func(*args, **kwargs) # Return the value of the function
+    return wrapper
+
+@do_twice
+def greet(name):
+    print(f"Hello {name}")
+    return f"Finished greeting {name}"
+
+result = greet("Alice")
+print(result)
+
+#preserv metadata
+import functools
+
+def preserve_metadata(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+    return wrapper
+
+@preserve_metadata
+def original_function():
+    """This is a docstring."""
+    pass
+
+print(original_function.__name__) # Output: original_function
